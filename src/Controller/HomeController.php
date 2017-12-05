6fa677c4b45286e41d3f998ecc4ae43db7d4f196
Craig Rayner <craig@craigrayner.com>
2017-12-05 13:38:16 +1100

    Add Templating Package

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,17 +1,17 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends BusybeeController
 {
 
 	/**
-	 * @Route('/', name='home')
+	 * @Route("/", name="home")
 	 */
 	public function home()
 	{
-
-
+		return $this->render('base.html.twig');
 	}
 }
