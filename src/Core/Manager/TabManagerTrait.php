75b1569852a59de843745303e19849c4c4144902
craigray <craig@craigrayner.com>
2018-04-27 12:03:59 +1000

    Allow Tab Display call to handle string or array.
    Signed-off-by: craigray <craig@craigrayner.com>

--- src/Core/Manager/TabManagerTrait.php
+++ src/Core/Manager/TabManagerTrait.php
@@ -5,17 +5,28 @@ trait TabManagerTrait
 {
     /**
      * Use this method as a callable to test if the tab is to be displayed.
-     * @param string $method
+     * @param string|array $method
      * @return bool
      */
-    public function isDisplay(string $method = ''): bool
+    public function isDisplay($method = []): bool
     {
-        if (empty($method))
-            return true;
-        if (method_exists($this, $method))
-            return $this->$method();
+        if (is_string($method)) {
+            if (empty($method))
+                return true;
+            if (method_exists($this, $method))
+                return $this->$method();
 
-        return false;
+            return false;
+        }
+        if (is_string($method['method']) && is_array($method['with']))
+        {
+            if (method_exists($this, $method['method'])) {
+                $func = $method['method'];
+                return $this->$func($method['with']);
+            }
+            return false;
+        }
+        throw new \InvalidArgumentException('The arguments passed to the tab display test must be an array = [method => name, with => [parameterArray]]');
     }
 
 }
