518997811339c4408f4f441d768c076522888642
craigray <craig@craigrayner.com>
2018-05-17 12:37:34 +1000

    DataFixtures
    Signed-off-by: craigray <craig@craigrayner.com>

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Hillrange\Security\HillrangeSecurityBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
