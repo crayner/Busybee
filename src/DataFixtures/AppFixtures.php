2856f5f361f1d85ab5bac6739b8344f02b596cd9
craigray <craig@craigrayner.com>
2018-05-17 12:35:14 +1000

    DataFixtures
    Signed-off-by: craigray <craig@craigrayner.com>

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * This file is part of the Busybee Project.
+ *
+ * (c) Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 17/05/2018
+ * Time: 09:40
+ */
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Finder\Finder;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // Bundle to manage file and directories
+        $finder = new Finder();
+        $finder->in(__DIR__ . '/SQL/App');
+        $finder->name('*.sql');
+        $finder->files();
+        $finder->sortByName();
+
+        foreach( $finder as $file ){
+            echo "Importing: {$file->getBasename()} " . PHP_EOL;
+
+            $sql = $file->getContents();
+
+            $manager->getConnection()->exec($sql);  // Execute native SQL
+
+            $manager->flush();
+        }
+    }
+}
